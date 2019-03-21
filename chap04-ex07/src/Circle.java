f45075b3c6ac612852c9aa13db094f79a87d1dcb
USER <USER@AMP-22>
2019-03-21 15:32:32 +0900

    commit

---
+++ chap04-ex07/src/Circle.java
@@ -0,0 +1,19 @@
+
+public class Circle {
+	int radius;
+	
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+	public double getArea() {
+		return 3.14 * radius * radius;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
