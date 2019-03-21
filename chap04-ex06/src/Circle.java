8eeb706bb45216761610bf8f154d1cba19a2b4c5
USER <USER@AMP-22>
2019-03-21 15:31:38 +0900

    commit

---
+++ chap04-ex06/src/Circle.java
@@ -0,0 +1,18 @@
+
+public class Circle {
+	int redius;
+	
+	public Circle(int redius) {
+		this.redius = redius;
+		
+	}
+	public double CirecleArray(){
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
