cfc75d9b73f76abadca53a586e582a0eab10ef0e
USER <USER@AMP-22>
2019-03-28 15:34:21 +0900

    commit

---
+++ chap04-pro11/src/Rectangle.java
@@ -0,0 +1,43 @@
+
+public class Rectangle {
+	private int x, y, width, height;
+
+	public Rectangle(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	public void show() {
+		System.out.print("(" + x + "." + y + ")에서");
+		System.out.println("크기가 " + width + "x" + height + "인 사각형");
+	}
+
+	public int square() {
+		return width * height;
+	}
+
+	public boolean contains(Rectangle r) {
+		if (x < r.x && y < r.y
+				&& x + width > r.x + r.width 
+				&&y + height > r.y + height)return true;
+		else
+			return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle r = new Rectangle(2,2,8,7);
+		Rectangle s = new Rectangle(5,5,6,6);
+		Rectangle t = new Rectangle(1,1,10,10);
+		r.show();
+		System.out.println("s의 면적은 " + s.square());
+		if (t.contains(r))
+			System.out.println("t는 r를 포함합니다");
+		if (t.contains(s))
+			System.out.println("t는 s를 포함합니다");
+
+	}
+
+}
