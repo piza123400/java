f9415e99e15afe83fbda99a2c8ec174c24ea70f6
USER <USER@AMP-22>
2019-05-16 10:50:19 +0900

    commit

---
+++ 2019_05_16_1/src/math.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+class Add {
+	public static int add(int i, int j) {
+
+		return i + j;
+	}
+}
+
+class Sub {
+	public static int sub(int i, int j) {
+
+		return i - j;
+	}
+}
+
+class Mul {
+	public static int mul(int i, int j) {
+	
+		return i * j;
+	}
+}
+
+class Div {
+	public static int div(int i, int j) {
+		
+		return i / j;
+	}
+}
+
+public class math {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("두 수와 연산자를 적으시오 ");
+		int i = scanner.nextInt();
+		int j = scanner.nextInt();
+		
+		char keycode = scanner.next().charAt(0);
+
+		switch (keycode) {
+		case '+':
+			
+			System.out.println(Add.add(j, j));
+
+			break;
+
+		case '-':
+			System.out.println(Sub.sub(j, j));
+			
+			break;
+			
+		case '*':
+			
+			System.out.println(Mul.mul(j, j));
+			
+			break;
+			
+		case '/':
+			
+			System.out.println(Div.div(j, j));
+			
+			break;
+			
+		default:
+			System.out.println("error");
+			break;
+		}
+		scanner.close();
+	}
+}
